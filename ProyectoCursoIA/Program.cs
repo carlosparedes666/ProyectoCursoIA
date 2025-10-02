30512b58dca00128e1a42db46574dd58323af6cf
Carlos Paredes <120127498+carlosparedes666@users.noreply.github.com>
2025-10-02 12:07:34 -0600

    Update database name in connection string

--- ProyectoCursoIA/Program.cs
+++ ProyectoCursoIA/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using ProyectoCursoIA.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
